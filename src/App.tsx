bf0512976fa4a2a388035aa955f6e8c16ca90642
sammykirigha <dkirigha18@gmail.com>
2023-02-15 18:45:16 +0300

    added more files

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello learning github actions</h2>
         <p>I love devops</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Devops is interesting</h1>
       </header>
     </div>
   );
